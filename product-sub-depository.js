e00040168fa069095baace31d43555d8d9bf2931
Ysh-ee <rmsid0121@gmail.com>
2023-02-06 10:45:29 +0900

    added anim2

---
+++ product-sub-depository.js
@@ -0,0 +1,94 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// gsap.utils.toArray(".panel").forEach((panel, i) => {
+//   ScrollTrigger.create({
+//     trigger: panel,
+//     start: "top top", 
+//     pin: true, 
+//     pinSpacing: false 
+//         // markers:true    //@@@
+//   });
+// });
+
+gsap.from(".wrapper",{
+    scrollTrigger:{
+        trigger:".wrapper",
+        start:"top top",
+        // end:"15% bottom",
+        scrub:2,
+        pinSpacing: false,
+        pin: true, 
+
+        // markers:true    //@@@
+    },
+    
+});
+
+// ScrollTrigger.create({
+//     snap: 1 / 4 // snap whole page to the closest section!
+//   });
+
+let scrollTween = gsap.to(".hori-item-group", {
+    xPercent: -15,
+    ease: "none", // <-- IMPORTANT!
+    scrollTrigger: {
+      trigger: ".hori-scroll",
+      pin: true,
+      scrub: 0.1,
+      //snap: directionalSnap(1 / (sections.length - 1)),
+      end: "+=200",
+    //   markers:true    //@@@
+
+    }
+  });
+
+
+//   ------------------scrollchange---------
+
+let blocks = gsap.utils.toArray("block"),
+    currentBlock = blocks[0];
+
+// gsap.defaults({overwrite: 'auto', duration: 0.3});
+
+// stretch out the body height according to however many sections there are. 
+// gsap.set("scroll-change-group", {height: (blocks.length * 100) + "%"});
+gsap.from(".scroll-change-group",{
+    scrollTrigger:{
+        trigger:".scroll-change-group",
+        start:"top top",
+        end:"bottom top",
+        scrub:20,
+        // pinSpacing: false,
+        pin: true, 
+
+        // markers:true    //@@@
+    },
+    
+});
+// create a ScrollTrigger for each section
+blocks.forEach((block, i) => {
+  ScrollTrigger.create({
+    // use dynamic scroll positions based on the window height (offset by half to make it feel natural)
+    trigger:".scroll-change-group",
+    // start: () => (3+i - 0.1) * (innerHeight/3*(i+3) ),
+    // end: () => (3+i + 0.1) * (innerHeight/3 ),
+    // start:"top top",
+    start: () => 3 * innerHeight + ((i-1)*150) +40,
+    // 
+    end: () => 3 *innerHeight + ((i-1)*150) + 140 ,
+    // start: () => (i - 0.3) * innerHeight + 1000,
+    // end: () => (i + 0.3) * innerHeight +1000,
+    // when a new section activates (from either direction), set the section accordinglyl.
+    // pin: true,
+    // markers:true,    //@@@
+    onToggle: self => self.isActive && setSection(block)
+  });
+});
+
+function setSection(newSection) {
+  if (newSection !== currentBlock) {
+    gsap.to(currentBlock, { autoAlpha: 0})
+    gsap.to(newSection, { autoAlpha: 1});
+    currentBlock = newSection;
+  }
+}
---
+++ video-page.js
@@ -0,0 +1,18 @@
+const videoPage_1 = document.querySelector("#video-page");
+
+let tl1 = gsap.timeline({
+    scrollTrigger: {
+    //   trigger: ".video-box",
+      start: "top top",
+      end: "10000% top",
+      scrub: true,
+      pin: ".video-box",
+    //   markers: true,    //@@@
+      // onEnter: () => video.play(),
+    //   duration:5000
+    }
+  });
+
+  videoPage_1.onloadedmetadata = function () {
+    tl1.to(videoPage_1, { currentTime: videoPage_1.duration });
+  };
