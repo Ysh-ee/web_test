b089b6caf115be2a1188f148b025aa34d3643b34
Ysh-ee <rmsid0121@gmail.com>
2023-02-16 11:25:22 +0900

    update 5

--- product-01.js
+++ product-01.js
@@ -122,8 +122,8 @@ blocks.forEach((block, i) => {
     // start: () => (3+i - 0.1) * (innerHeight/3*(i+3) ),
     // end: () => (3+i + 0.1) * (innerHeight/3 ),
     // start:"top top",
-    start: () => 3 * innerHeight + ((i+2)*1350) +150,
-    end: () => 3 *innerHeight + ((i+2)*1350) +300,
+    start: () => 3 * innerHeight + ((i+2)*1050) +150,
+    end: () => 3 *innerHeight + ((i+2)*1050) +300,
     // start: () => (i - 0.3) * innerHeight + 1000,
     // end: () => (i + 0.3) * innerHeight +1000,
     // when a new section activates (from either direction), set the section accordinglyl.
