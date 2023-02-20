70ca0067f32d173a86dfb9247216bb041ad25464
Ysh-ee <rmsid0121@gmail.com>
2023-02-20 16:39:34 +0900

    updatee

--- competitive-1.js
+++ competitive-1.js
@@ -4,10 +4,11 @@ gsap.from(".section-1",{
     scrollTrigger:{
         trigger:".scroll-pin-1",
         start:"50% center",
-        end:"190% center",
+        // end:"bottom center",
+        end:"185% center",
         scrub:20,
         pinSpacing: false,
         pin: ".scroll-pin-1", 
-        // markers:true    //@@@
+        markers:true    //@@@
     },
 });
